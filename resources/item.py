a2a04b34f37660a19d598a7b1c20182479a46288
Tobias Pucci Romero <tobias.pucci@mercadolibre.com>
2024-01-08 16:05:55 -0300

    SQLAlchemy addition

--- resources/item.py
+++ resources/item.py
@@ -1,16 +1,17 @@
 import uuid
 
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import items
+from schemas import ItemSchema
 
 blp = Blueprint('item', __name__, description='Item operations')
 
 
 @blp.route('/item/<string:item_id>')
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -24,13 +25,9 @@ class Item(MethodView):
         except KeyError:
             return {"message": "Item not found"}, 404
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
 
@@ -42,17 +39,20 @@ class Item(MethodView):
 
 
 @blp.route("/item")
-class Item(MethodView):
+class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
-    def post(self):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemSchema)
+    @blp.response(200, ItemSchema)
+    def post(self, item_data):
+        for item in items.values():
+            if (
+                    item_data["name"] == item["name"]
+                    and item_data["store_id"] == item["store_id"]
+            ):
+                abort(400, message="Item already exists.")
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id}
         items[item_id] = item
